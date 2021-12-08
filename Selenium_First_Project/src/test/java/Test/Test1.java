2d5965b92ca713a5ce0a5a7178650514e64bfb1b
ezgi.colak <ezgi.colak97@gmail.com>
2021-12-08 08:25:00 +0300

    Lesson_3

--- Selenium_First_Project/src/test/java/Test/Test1.java
+++ Selenium_First_Project/src/test/java/Test/Test1.java
@@ -1,6 +1,8 @@
 package Test;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Test1 {
@@ -10,7 +12,14 @@ public class Test1 {
         System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
-        driver.get("https://demoqa.com/");
+        driver.get("https://demoqa.com/text-box"); // sayfayı açar
+        driver.manage().window().maximize(); // pencereyi büyütmek için kullanılır.
+
+        WebElement nameElement = driver.findElement(By.id("userName")); // elementi bulur
+        nameElement.click(); // elemente tıklar
+        nameElement.sendKeys("Ezgi"); //elementin içini doldurur
+
+        
 
 
 
